2576948e253195e83838a78272786286df75be0f
Saad E <21321555+se447@users.noreply.github.com>
2019-03-21 20:39:59 -0400

    read numbers until user types -1

--- src/LoopsEndingRemembering.java
+++ src/LoopsEndingRemembering.java
@@ -7,5 +7,20 @@ public class LoopsEndingRemembering {
         
         Scanner reader = new Scanner(System.in);
 
+        /*
+        Create a program that asks the user to input numbers (integers). 
+        The program prints "Type numbers” until the user types the number -1. 
+        When the user types the number -1, the program prints "Thank you and see you later!" and ends.
+        */
+        System.out.println("Type numbers: ");
+        int number = Integer.parseInt(reader.nextLine());
+        
+        while (number != -1) {
+            System.out.println(number);
+            
+            number = Integer.parseInt(reader.nextLine());        
+        }
+        
+        System.out.println("Thank you and see you later!");
     }
 }
