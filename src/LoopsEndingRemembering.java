92abd40a9fc968a3861126e95145acc6d6ddd5ce
Saad E <21321555+se447@users.noreply.github.com>
2019-03-21 20:44:41 -0400

    Calculated count of numbers entered into program

--- src/LoopsEndingRemembering.java
+++ src/LoopsEndingRemembering.java
@@ -11,20 +11,28 @@ public class LoopsEndingRemembering {
         Create a program that asks the user to input numbers (integers). 
         The program prints "Type numbers” until the user types the number -1. 
         When the user types the number -1, the program prints "Thank you and see you later!" and ends.
+        
+        Develop your number reading program by adding the following feature: 
+        the program should print the sum of the numbers entered by the user (without the number -1).
+        
+        The program should print how many numbers the user typed (without the number -1).
         */
         System.out.println("Type numbers: ");
         int number = Integer.parseInt(reader.nextLine());
         
         
         int sum = 0;
+        int count = 0;
         while (number != -1) {
             System.out.println(number);
             
             sum += number;
+            count += 1; //counts numbers read into program
             number = Integer.parseInt(reader.nextLine());        
         }
         
         System.out.println("Thank you and see you later!");
         System.out.println("The sum is " + sum);
+        System.out.println("How many numbers: " + count);
     }
 }
